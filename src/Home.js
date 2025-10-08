7c0721d704ebde8dd9bd5e641eb8e6f412b2d6d7
Tanairahul <tanwarrahul39178@gmail.com>
2025-10-08 23:10:27 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -12,7 +12,7 @@ function Home() {
     <Router basename="/React-project">
       <div className="p-5 border border-gray-400 flex items-center justify-between">
         <nav style={{paddingRight:"10px", margin:"20px", display:"flex", alignItems:"center", border:"1px solid black" }}>
-          <img src={ph} style={{height:"50px", width:"50px", padding:"10px"}} alt="rahul" />
+       <img src={process.env.PUBLIC_URL + '/ph.png'} alt="rahul" />
           <Link to="/signup" style={{marginRight:"10px"}}><button>Signup</button></Link>
           <Link to="/login"><button>Login</button></Link>
         </nav>
