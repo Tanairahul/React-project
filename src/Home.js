c6e6d7578599af788e5a30b7128a2c850a18675f
Tanairahul <tanwarrahul39178@gmail.com>
2025-10-10 22:43:38 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,44 @@
 import React from "react";
-import { Link, Routes, Route, BrowserRouter as Router } from "react-router-dom";
-import Signup from "./Signup.js";
+import { Link, Routes, Route, HashRouter as Router } from "react-router-dom";
+import Signup from "./Signup";
 import Login from "./Login";
-import ph from "./ph.png";
-import './App.css';
-import './index.css';
 import Dashboard from "./Dashboard";
+import ph from "./ph.png";
+import "./App.css";
+import "./index.css";
 
 function Home() {
   return (
-    <Router basename="/React-project">
-      <div className="p-5 border border-gray-400 flex items-center justify-between">
-        <nav style={{paddingRight:"10px", margin:"20px", display:"flex", alignItems:"center", border:"1px solid black" }}>
-         <img src={ph} style={{height:"50px",width:"50px",padding:"10px"}} alt="rahul" />
-          <Link to="/signup" style={{marginRight:"10px"}}><button>Signup</button></Link>
-          <Link to="/login"><button>Login</button></Link>
+    <Router>
+      <div style={{ maxWidth: "900px", margin: "auto" }}>
+        {/* Navigation Bar */}
+        <nav
+          style={{
+            padding: "10px 20px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            border: "1px solid #ccc",
+            borderRadius: "8px",
+            marginBottom: "20px",
+          }}
+        >
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <img
+              src={ph}
+              alt="Logo"
+              style={{ height: "50px", width: "50px", marginRight: "15px" }}
+            />
+            <Link to="/signup">
+              <button style={{ marginRight: "10px" }}>Signup</button>
+            </Link>
+            <Link to="/login">
+              <button>Login</button>
+            </Link>
+          </div>
         </nav>
 
+        {/* Routes */}
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
